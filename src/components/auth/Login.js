bf0105392bca109dddc26ccd753cc875762e19f7
Vlad <vlad220994@gmail.com>
2019-04-19 08:28:23 +0300

    created Login.js

---
+++ src/components/auth/Login.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+import usersData from '../../data/users-data';
+import './Login.scss';
+
+localStorage.setItem('usersData', JSON.stringify(usersData));
+
+const Login = () => (
+  <form className="login-form">
+    <div className="logo">
+      <i className="fa fa-camera" />
+    </div>
+    <div>
+      <h1>InstaClone</h1>
+    </div>
+    <div className="form-group">
+      <input type="email" placeholder="Email" />
+    </div>
+    <div className="form-group">
+      <input type="password" placeholder="Password" />
+    </div>
+    <button type="submit" className="btn btn-primary btn-left">Login</button>
+    <div className="to-registration">
+      First time on InstaClone?
+      {' '}
+      <Link to="/register">Registration</Link>
+    </div>
+  </form>
+);
+
+export default Login;
